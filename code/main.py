85dfe39cfbc4deae2ce7de807ec05be14652d2a7
Olesya Nikolaeva <63335416+olesyani@users.noreply.github.com>
2021-03-25 17:34:54 +0300

    mistake fixed

--- code/main.py
+++ code/main.py
@@ -146,7 +146,7 @@ if __name__ == "__main__":
             title_array.append(file.replace('_query.txt', ''))
 
     for i in range(len(query_img_array)):
-        query_img = query_img_array
+        query_img = query_img_array[i]
         print(query_img)
         file_name = 'data/' + descriptorsFinder + '/' + query_img[:-4] + '_des.txt'
         des1 = np.loadtxt(file_name).astype('float32')
