6c30a3bb0ede8a368779d672409b22a29e263aaa
Olesya Nikolaeva <63335416+olesyani@users.noreply.github.com>
2021-06-18 06:19:15 +0300

    Update compute_ap.py

--- metrics/compute_ap.py
+++ metrics/compute_ap.py
@@ -1,7 +1,5 @@
 import sys
 
-from typing import List
-
 
 def load_list(fname: str):
     return [e.strip() for e in open(fname, 'r').readlines()]
@@ -10,11 +8,11 @@ def load_list(fname: str):
 def compute_ap(positive, negative, ranked_list):
     intersect_size, old_recall, ap = 0.0, 0.0, 0.0
     old_precision, j = 1.0, 1.0
-    
+
     for img in ranked_list:
         if img in negative:
             continue
-            
+
         if img in positive:
             intersect_size += 1.0
 
@@ -31,14 +29,15 @@ def compute_ap(positive, negative, ranked_list):
 
 def compute(arg):
     ranked_list_file = 'ranked_list.txt'
-    
+
     try:
         ranked_list = load_list(ranked_list_file)
-        pos_set = list(set(load_list("%s_good.txt" % arg) + load_list("%s_ok.txt" % arg)))
+        pos_set = list(set(load_list("%s_good.txt" % arg) 
+                           + load_list("%s_ok.txt" % arg)))
         junk_set = load_list("%s_junk.txt" % arg)
 
         return str(compute_ap(pos_set, junk_set, ranked_list))
-    
+
     except IOError as e:
         print("IO error while opening files. %s" % e)
         sys.exit(1)
